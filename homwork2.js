1e7a666418818c2a3f605ec22a70bcbd442f297c
nicolasallende <allenden96@gmail.com>
2025-06-02 12:14:25 -0300

    "Add stringify after doing some basic tests"

--- homwork2.js
+++ homwork2.js
@@ -5,3 +5,20 @@ function invertBoolean(bool){
     }
     return !bool
 }
+
+function stringifyValue(value){
+    if(typeof value === "object" && value !== null){
+        return JSON.stringify(value)
+    }
+    return String(value)
+}
+
+/*
+var aux2
+var aux = true
+var value = { x: [1, { y: true }] }
+console.log(invertBoolean(aux))
+console.log(stringifyValue(value))
+console.log(stringifyValue(aux))
+console.log(stringifyValue(aux2))
+*/
