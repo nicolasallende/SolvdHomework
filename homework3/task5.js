b19ff95a6c249f2525947f650ea4927b19629247
nicolasallende <allenden96@gmail.com>
2025-06-17 00:12:30 -0300

    Make fibonacci generator

--- homework3/task5.js
+++ homework3/task5.js
@@ -7,15 +7,13 @@ function lazyMap(arr,mapFunc){
             if(index < arr.length){
                 const value = mapFunc(arr[index]);
                 index++;
-                //tha name has to be "done" otherwise the iterator for ..of doesn't work
+                //the name has to be "done" otherwise the iterator for ..of doesn't work
                 return {value, done: false};
             }else{
                 return {value: undefined, done: true};
             }
         },
-
-        //this makes the object capable f being used with for ...of  
-
+        //this makes the object capable of being used with for ...of  
         [Symbol.iterator] : function(){
             return this;
         }
@@ -24,10 +22,37 @@ function lazyMap(arr,mapFunc){
 
 
 
+function fibonacciGenerator(){
+    let a = 0;
+    let b = 1;
+
+    return {
+        next: function(){
+            const current = a; 
+            [a, b] = [b, a+b];
+            //the name has to be "done" otherwise the iterator for ..of doesn't work
+            return {value: current, done: false}
+        },
+        //this makes the object capable of being used with for ...of  
+        [Symbol.iterator] : function(){
+            return this;
+        }
+    };
+}
 
 
 //Test lazyMap
-
+/*
 for (const val of lazyMap([1, 2, 3], x => (x * 2))) {
     console.log(val);
   }
+*/
+//test FibonacciGenerator
+/*
+const fib = fibonacciGenerator();
+
+for (const num of fib) {
+  console.log(num);
+  if (num > 10) break;  
+}
+*/
