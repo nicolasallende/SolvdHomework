c53cb2e98b5d3013e2759cc9dabe42cc9b5a9b53
nicolasallende <allenden96@gmail.com>
2025-07-03 00:47:18 -0300

    task1 HM5

---
+++ homwork5/Task1.js
@@ -0,0 +1,24 @@
+function customFilterUnique(arr, callback) {
+    const seen = new Set();
+    return arr.filter(item => {
+      const key = callback(item); 
+      if (seen.has(key)) return false; 
+      seen.add(key); 
+      return true;
+    });
+};
+
+//simple test--------------------------------------
+/*
+const users = [
+    { id: 1, name: "Alice", email: "asdada@gma.com" },
+    { id: 2, name: "Bob", email: "someting@gmail.com" },
+    { id: 1, name: "Alice Again", email: "" },
+    { id: 3, name: "Charlie", email: "" },
+];
+  
+const uniqueById = customFilterUnique(users, user => user.id);
+console.log(uniqueById);
+const uniqueById2 = customFilterUnique(users, user => user.email);
+console.log(uniqueById2);
+*/
