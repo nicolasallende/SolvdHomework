22723b2c52001ba76a3eab471329a22fe8c42e8f
nicolasallende <allenden96@gmail.com>
2025-08-05 12:03:43 -0300

    Insert binary tree

--- homework9/Task1.js
+++ homework9/Task1.js
@@ -59,10 +59,51 @@ class Queue {
         this.#items = [];
     }
 }
-  
+
+//Class used for the nodes of the binary tree
+
+class TreeNode {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+}
+
+
   class BinaryTree {
-    // Implement methods for inserting nodes, searching, traversing...
-  }
+    constructor(){
+        this.root = null
+    }
+
+    insert(value) {
+        const newNode = new TreeNode(value);
+    
+        if (!this.root) {
+          this.root = newNode;
+          return;
+        }
+    
+        let current = this.root;
+    
+        while (true) {
+          if (value < current.value) {
+            if (!current.left) {
+              current.left = newNode;
+              return;
+            }
+            current = current.left;
+          } else {
+            //if equal value we send it to the node on the right
+            if (!current.right) {
+              current.right = newNode;
+              return;
+            }
+            current = current.right;
+          }
+        }
+    }
+}
   
   class Graph {
     // Implement methods for adding vertices, edges, DFS, BFS...
